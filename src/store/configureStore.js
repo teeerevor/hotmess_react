11c58445fdf81f571a0a0ab3b70fd762fd377079
Trev <teeerevor@gmail.com>
2017-03-02 19:37:53 +1100

    fix repeat dispatch issue

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -25,10 +25,10 @@ function configureStoreDev(initialState) {
 
     // Redux middleware that spits an error on you when you try to mutate your state either inside a dispatch or between dispatches.
     reduxImmutableStateInvariant(),
+    thunk,
     createLogger({ collapsed: true }),
     // thunk middleware can also accept an extra argument to be passed to each thunk action
     // https://github.com/gaearon/redux-thunk#injecting-a-custom-argument
-    thunk,
   ];
 
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add support for Redux dev tools
