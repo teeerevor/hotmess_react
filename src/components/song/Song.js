72f2d1f59a0f13998a474b73a84fdba9ce1d9dbe
Trev <teeerevor@gmail.com>
2017-12-08 13:09:07 +1100

    updating song component

--- src/components/song/Song.js
+++ src/components/song/Song.js
@@ -1,62 +1,58 @@
 import React, {PropTypes} from 'react';
-import { ScrollElement } from 'react-scroll';
+//import { ScrollElement } from 'react-scroll';
+
+import SongAudio          from '../SongAudio';
 import IconPlus           from '../svgs/IconPlus';
 import IconUparrow        from '../svgs/IconUparrow';
 import IconPlay           from '../svgs/IconPlay';
 import IconTick           from '../svgs/IconTick';
-import SongAudio          from '../SongAudio';
+
 import styles             from './Song.styl';
 import btns               from '../../styles/buttons.styl';
 
-class Song extends React.Component {
-  renderAudio = (song, open) => {
-    if( open )
-      return (<div className={styles.songAudio}>
-                <SongAudio song={song} />
-              </div>);
+const Song = ({
+  song,
+  sortBy,
+  shortlisted,
+  isOpen,
+  onToggleSongView,
+  onShortlistTop,
+  onShortlist,
+}) => {
+  let className = styles.song;
+  if (isOpen) className = styles.open;
+  if (shortlisted) className = styles.shortlisted;
+  if (isOpen && shortlisted) className = styles.openAndShortlisted;
+
+  let title = song.artist;
+  let additional = song.name;
+  if(sortBy === 'song'){
+    title = song.name;
+    additional = song.artist;
   }
 
-  arrangeSongInfo(song, sortBy) {
-    if(sortBy === 'song')
-      return (<span className={styles.text}>
-                <b> {song.name} </b>
-                &nbsp;-&nbsp;
-                {song.artist}
-              </span>);
-    else
-      return (<span className={styles.text}>
-                <b>{song.artist}</b>
-                &nbsp;-&nbsp;
-               {song.name}
-              </span>);
-  }
-
-  render() {
-    const {song, sortBy, shortlisted, isOpen} = this.props;
-    const id = this.props.song.id;
-
-    let className = styles.song;
-    if (isOpen) className = styles.open;
-    if (shortlisted) className = styles.shortlisted;
-    if (isOpen && shortlisted) className = styles.openAndShortlisted;
-
-    return (
-      <li className={className} name={'song'+id} data-id={id}>
-        <div className={styles.tag} onClick={this.props.onToggleSongView}>
-          {this.arrangeSongInfo(song, sortBy)}
-          {this.renderAudio(song, isOpen)}
-          <IconPlay className={styles.iconPlay} />
-          <IconTick className={styles.iconSelected} />
-        </div>
-        <button className={btns.circleButton} onClick={this.props.onShortlistTop} > <IconUparrow /> </button>
-        <button className={btns.circleButton} onClick={this.props.onShortlist} > <IconPlus /> </button>
-      </li>
-    );
-  }
-}
+  return (
+    <li className={className} name={'song'+song.id} data-id={song.id}>
+      <div className={styles.tag} onClick={onToggleSongView}>
+        <span className={styles.text}>
+          <b>{title}</b> - {additional}
+        </span>
+        {isOpen &&
+            <div className={styles.songAudio}>
+              <SongAudio song={song} />
+            </div>
+        }
+        <IconPlay className={styles.iconPlay} />
+        <IconTick className={styles.iconSelected} />
+      </div>
+      <button className={btns.circleButton} onClick={onShortlistTop} > <IconUparrow /> </button>
+      <button className={btns.circleButton} onClick={onShortlist} > <IconPlus /> </button>
+    </li>
+  );
+};
 
 Song.propTypes = {
-  song: PropTypes.object.isRequired,
+  song: PropTypes.object,
   sortBy: PropTypes.string,
   isOpen: PropTypes.bool,
   shortlisted: PropTypes.bool,
@@ -65,4 +61,5 @@ Song.propTypes = {
   onShortlist: PropTypes.func,
 };
 
-export default ScrollElement(Song);
+export default Song;
+//export default ScrollElement(Song);
